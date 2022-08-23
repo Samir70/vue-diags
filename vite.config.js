1f3c311143ffa5d6f8736da03296af5aba915fa4
Samir70 <sbetmouni@talktalk.net>
2022-08-23 06:34:16 +0100

    set up vite.config for being a library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+// copied from https://vitejs.dev/guide/build.html#library-mode
+
+module.exports = defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'q-show',
+      fileName: (format) => `q-show.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
