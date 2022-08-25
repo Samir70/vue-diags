cbd08fbb7f78dd2f61c3badb0595d485b2dc2f0f
Samir70 <sbetmouni@talktalk.net>
2022-08-25 22:27:06 +0100

    enable testing of parameters in circle.withRadius

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue"
 import * as diags from "./main"
 
+const radius = ref(5);
+const penCol = ref("black")
+const fillCol = ref("white")
 const tests = {
-  circleWithRadiusTest: { r: 12, penColour: "black", fillColour: "white" }
+  circleWithRadiusTest: () => {
+    return { r: Number(radius.value) || 13, penColour: penCol.value, fillColour: fillCol.value }
+  }
 }
 </script>
 
 <template>
-  <component v-bind:is="diags.circles.withRadius" v-bind:params="tests.circleWithRadiusTest" />
+  <component v-bind:is="diags.circles.withRadius" v-bind:params="tests.circleWithRadiusTest()" />
+  <div>
+    <label for="radius">Radius</label>
+    <input type="text" v-model="radius" id="radius" />
+  </div>
+  <div>
+    <label for="pencol">Pen Colour</label>
+    <input type="text" v-model="penCol" id="pencol" />
+  </div>
+  <div>
+    <label for="fillcol">Fill Colour</label>
+    <input type="text" v-model="fillCol" id="fillcol" />
+  </div>
 </template>
 
 <style scoped>
+label {
+  padding: 0px 5px;
+}
 </style>
--- src/components/Circles/withRadius.vue
+++ src/components/Circles/withRadius.vue
@@ -5,10 +5,10 @@ const props = defineProps({
 </script>
 <template>
     <svg viewBox="0 0 400 400">
-        <circle cx="200" cy="200" r="160" v-bind:stroke="params.penColour" stroke-width="5"
+        <circle cx="200" cy="200" r="195" v-bind:stroke="params.penColour" stroke-width="5"
             v-bind:fill="params.fillColour" />
-        <line x1="200" y1="200" x2="360" y2="200" v-bind:stroke="params.penColour" stroke-width="4" />
-        <text x="260" y="190">{{ params.r }}</text>
+        <line x1="200" y1="200" x2="395" y2="200" v-bind:stroke="params.penColour" stroke-width="4" />
+        <text x="280" y="190">{{ params.r }}</text>
     </svg>
     <p>CircleWithRadius: {{ props.params }}</p>
 </template>
