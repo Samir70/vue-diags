2fe64fe06dfd29cf6a24b8e59ac5a909d9f9ebc2
Samir70 <sbetmouni@talktalk.net>
2022-09-28 12:58:32 +0100

    add withRadius and withDiameter to circles

--- src/components/Circles/withDiameter.vue
+++ src/components/Circles/withDiameter.vue
@@ -5,21 +5,12 @@ const props = defineProps({
 const rotation = `rotate(${props.params.angle})`
 </script>
 <template>
-    <svg viewBox="0 0 400 400" v-bind:transform="rotation" >
+    <svg viewBox="0 0 400 400" v-bind:transform="rotation"  width="200px">
         <circle cx="200" cy="200" r="195" v-bind:stroke="params.penColour" stroke-width="5"
             v-bind:fill="params.fillColour" />
         <line x1="5" y1="200" x2="395" y2="200" v-bind:stroke="params.penColour" stroke-width="4" />
-        <text x="190" y="190">{{ params.r }}</text>
+        <text font-size="3em" x="190" y="190">{{ params.d }}</text>
     </svg>
-    <p>circle.withDiameter: {{ props.params }}</p>
 </template>
 <style>
-svg {
-    height: 30vh;
-    width: auto;
-}
-
-text {
-    font-size: 3em;
-}
 </style>
--- src/components/Circles/withRadius.vue
+++ src/components/Circles/withRadius.vue
@@ -2,23 +2,15 @@
 const props = defineProps({
     params: Object
 })
+const rotation = `rotate(${props.params.angle})`
 </script>
 <template>
-    <svg viewBox="0 0 400 400">
+    <svg viewBox="0 0 400 400" width="200px" v-bind:transform="rotation" >
         <circle cx="200" cy="200" r="195" v-bind:stroke="params.penColour" stroke-width="5"
             v-bind:fill="params.fillColour" />
         <line x1="200" y1="200" x2="395" y2="200" v-bind:stroke="params.penColour" stroke-width="4" />
-        <text x="280" y="190">{{ params.r }}</text>
+        <text font-size="3em" x="280" y="190">{{ params.r }}</text>
     </svg>
-    <p>CircleWithRadius: {{ props.params }}</p>
 </template>
 <style>
-svg {
-    height: 30vh;
-    width: auto;
-}
-
-text {
-    font-size: 3em;
-}
 </style>
